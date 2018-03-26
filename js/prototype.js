bc7cdc6f95f1787d13a84a82e5b58300d1e9ea80
Marcus Kopp <marcus.kopp86@gmail.com>
2018-03-26 12:00:45 +0200

    prototype.js: Added functionality for ExpandAll and CollapseAll buttons Functionality is needed to improve interface usability by enabling admin to show all super topics on demand, or collapse them when needed. Currently done by iterating over id{$i} and changing display attribute as necessary

--- js/prototype.js
+++ js/prototype.js
@@ -2,3 +2,21 @@ function toggleDiv(id) {
     var div = document.getElementById(id);
     div.style.display = div.style.display == "none" ? "inline-block" : "none";
 }
+
+function expandAll() {
+    for (i = 1; i < 7; i++){
+        toggleHelper(document.getElementById('id' + i), 'inline-block');
+    }
+}
+
+function collapseAll() {
+    for (i = 1; i < 7; i++){
+        toggleHelper(document.getElementById('id' + i), 'none');
+    }
+}
+
+function toggleHelper(div, value) {
+    if (div.style.display != value) {
+        div.style.display = value;
+    }
+}
