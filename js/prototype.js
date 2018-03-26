221673aea008b70b7a5cc3f4249b76e6eba683e8
Marcus Kopp <marcus.kopp86@gmail.com>
2018-03-26 16:22:08 +0200

    Design reworked Design prototype for the settings page. Changed stylings to clearly separate the different topics

--- js/prototype.js
+++ js/prototype.js
@@ -1,11 +1,11 @@
 function toggleDiv(id) {
     var div = document.getElementById(id);
-    div.style.display = div.style.display == "none" ? "inline-block" : "none";
+    div.style.display = div.style.display == "none" ? "flex" : "none";
 }
 
 function expandAll() {
     for (i = 1; i < 7; i++){
-        toggleHelper(document.getElementById('id' + i), 'inline-block');
+        toggleHelper(document.getElementById('id' + i), 'flex');
     }
 }
 
